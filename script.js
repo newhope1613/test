22da27925e8438eb61eed7b80460f1fe7dada673
Abdubek Bakdaulet <bakdauletabdubek@gmail.com>
2024-10-06 02:05:23 +0500

    third changes

--- script.js
+++ script.js
@@ -1,34 +1,15 @@
-// let a = +prompt('1');
-// b = +prompt('2');
-// c = +prompt('3');
-// let baha;
+let a = +prompt('1');
+b = +prompt('2');
+c = +prompt('3');
+let baha;
 
-// if (isNaN(a) || isNaN(b) || isNaN(c)) {
-//   baha = 'не все числа ведены вверно';
-// } else if (a > b && a > c) {
-//   baha = a;
-// } else if (b > c) {
-//   baha = b;
-// } else {
-//   baha = c;
-// }
-
-// for (let x = 0; x < 100; x++) {
-//   alert(x);
-// }
-
-let a = +prompt('введите 1-е число');
-let b = +prompt('введмите 2-е число');
-let c = +prompt('введите 3-е число');
-
-let number;
-hello
-
-
-if (a > b && a < c) {
-  console.log(a);
-} else if (b > a && b < c) {
-  console.log(b);
+if (isNaN(a) || isNaN(b) || isNaN(c)) {
+  baha = 'не все числа ведены вверно';
+} else if (a > b && a > c) {
+  baha = a;
+} else if (b > c) {
+  baha = b;
 } else {
-  console.log(c);
+  baha = c;
 }
+
